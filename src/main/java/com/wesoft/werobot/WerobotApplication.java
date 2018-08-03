942008f124ec15a9382f2314d74cc1adac4b8757
Daly You <daly.you@wesoft.com>
2018-08-03 09:11:56 +0800

    First commit

---
+++ src/main/java/com/wesoft/werobot/WerobotApplication.java
@@ -0,0 +1,12 @@
+package com.wesoft.werobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerobotApplication.class, args);
+	}
+}
---
+++ src/main/java/com/wesoft/werobot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.wesoft.werobot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String hello(@RequestParam(defaultValue = "test") String name, Model model){
+        model.addAttribute("message", "hello " + name);
+        return "resultPage";
+    }
+}
